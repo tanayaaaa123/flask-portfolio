a8ed8373e044c2f7a21f9911f75a1e96ac00944a
Tanaya <tanayaranade@gmail.com>
2025-05-11 23:55:02 +0530

    Added Experience section

--- static/js/script.js
+++ static/js/script.js
@@ -38,4 +38,19 @@ window.addEventListener('scroll', function () {
       }
     });
   });
+
+  
+document.querySelectorAll('.collapsible').forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('active');
+    const content = button.nextElementSibling;
+    if (button.classList.contains('active')) {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      content.style.maxHeight = null;
+    }
+  });
+});
+
+
   
